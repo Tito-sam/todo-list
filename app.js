61b352bb09f074881dbc4e33cec8ce7d26d752d6
Tito Samuel Mendez Orjuela <totitron20@gmail.com>
2023-01-26 12:51:01 -0500

    configurando el engine de views

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const path = require("path")
 const app = express();
 const PORT = 3000;
 
-var router = require("./src/router/router.js");
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'ejs');
+
+const todoRouter = require("./src/router/todo.router.js");
+const indexRouter = require('./src/router/index.router.js')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(indexRouter);
+app.use(todoRouter);
+
 
 console.log("Servidor en el puerto " + PORT);
 app.listen(PORT);
--- src/router/todo.router.js
+++ src/router/todo.router.js
@@ -2,10 +2,6 @@ const express = require("express");
 var router = express.Router();
 const {getTodo, getTodos, createTodo, updateTodo, deleteTodo} = require('../controller/controller.js')
 
-router.get('/', (req, res) => {
-    res.send("Hello World");
-})
-
 router.get('/todos', getTodos)
 router.post('/todos', createTodo)
 router.get('/todos/:id', getTodo)
